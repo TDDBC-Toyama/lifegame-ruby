fba9dd40c857e440671c701ef3f3aae313f680ed
Kunihiko Ito <kuni.110.92@gmail.com>
2017-03-05 16:58:12 +0900

    ループでの一定回数の盤面の出力を実装

--- main.rb
+++ main.rb
@@ -13,3 +13,14 @@ for i in 0..4 do
   end
   puts line
 end
+
+for times in 0..2
+  puts "#{times}=========="
+  for i in 0..4 do
+    line = ''
+    for j in 0..4 do
+      line += b[i][j]
+    end
+    puts line
+  end
+end
