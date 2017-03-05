2d07cab75e06d6787dd6bdae1fa27a7532120fa8
Kunihiko Ito <kuni.110.92@gmail.com>
2017-03-05 17:13:14 +0900

    生存・過疎・過密を実装
    初期状態は wikipedia "ライフゲーム" 記載の「ブリンカー」を参考にした
    (参考: https://ja.wikipedia.org/wiki/%E3%83%A9%E3%82%A4%E3%83%95%E3%82%B2%E3%83%BC%E3%83%A0)

--- main.rb
+++ main.rb
@@ -6,6 +6,10 @@ for i in 0..4 do
   end
 end
 
+b[2][1] = '■'
+b[2][2] = '■'
+b[2][3] = '■'
+
 for i in 0..4 do
   line = ''
   for j in 0..4 do
@@ -15,6 +19,29 @@ for i in 0..4 do
 end
 
 for times in 0..2
+  bb = [[],[],[],[],[]]
+  for i in 0..4 do
+    for j in 0..4 do
+      count = 0
+
+      bb[i][j] = b[i][j]
+
+      if b[i][j] == '■'
+        # 生存・過疎・過密の場合
+        count = 0
+        if b[i-1][j] == '■'; count += 1 end
+        if b[i][j-1] == '■'; count += 1 end
+        if b[i][j+1] == '■'; count += 1 end
+        if b[i+1][j] == '■'; count += 1 end
+        if count == 2
+          bb[i][j] = b[i][j]
+        else
+          bb[i][j] = '□'
+        end
+      end
+    end
+  end
+  b = bb
   puts "#{times}=========="
   for i in 0..4 do
     line = ''
